97dfa5c25427dd732462e4d6ade22315fdd9c3a9
Bohdan Szymanik <bohdan.szymanik@gmail.com>
2015-08-04 20:34:14 +1200

    Initial commit

---
+++ analysis.fsx
@@ -0,0 +1,11 @@
+﻿
+#r @"packages\FSharp.Data.2.2.5\lib\net40\FSharp.Data.dll"
+open FSharp.Data
+open System.IO
+
+type Stitch = JsonProvider<"c:\\temp\\stitching.json">
+let stitch = Stitch.Parse(File.ReadAllText("c:\\temp\\stitching.json"))
+
+stitch.Data
+|> Seq.iter (fun i -> 
+                    printfn "%A, %A, %A" i.CreatedTime i.Location i.Tags )
